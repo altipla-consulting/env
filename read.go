e334428e7287e55c0d5d9082368222397a76fb2e
Ernesto Alejo <ernestokarim@gmail.com>
2023-05-07 00:04:42 +0200

    Optionally read an env variable.

--- read.go
+++ read.go
@@ -11,19 +11,10 @@ import (
 // MustRead asserts an env variable exists and it is not empty. It reads its value.
 // If the value starts with "base64://" it will be decoded as well before returning.
 func MustRead(name string) string {
-	v := os.Getenv(name)
+	v := OptionalRead(name)
 	if v == "" {
 		panic(fmt.Sprintf("missing %s environment variable", name))
 	}
-
-	if strings.HasPrefix(v, "base64://") {
-		decoded, err := base64.StdEncoding.DecodeString(v[9:])
-		if err != nil {
-			panic(fmt.Sprintf("invalid base64 %s environment variable: %s", name, err.Error()))
-		}
-		v = string(decoded)
-	}
-
 	return v
 }
 
@@ -35,4 +26,19 @@ func MustReadJSON(name string, dest any) {
 	if err := json.Unmarshal([]byte(v), dest); err != nil {
 		panic(fmt.Sprintf("invalid json %s environment variable: %s", name, err.Error()))
 	}
+}
+
+// OptionalRead reads an env variable if it exists and it is not empty. It returns
+// the value. If the value starts with "base64://" it will be decoded as well before
+// returning.
+func OptionalRead(name string) string {
+	v := os.Getenv(name)
+	if strings.HasPrefix(v, "base64://") {
+		decoded, err := base64.StdEncoding.DecodeString(v[9:])
+		if err != nil {
+			panic(fmt.Sprintf("invalid base64 %s environment variable: %s", name, err.Error()))
+		}
+		v = string(decoded)
+	}
+	return v
 }
